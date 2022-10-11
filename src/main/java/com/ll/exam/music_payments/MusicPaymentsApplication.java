5a6aa508910b9ef9b621ed86c430654d7b262b4d
somang-lim <somangjuha@gmail.com>
2022-10-11 13:58:09 +0900

    setting

---
+++ src/main/java/com/ll/exam/music_payments/MusicPaymentsApplication.java
@@ -0,0 +1,13 @@
+package com.ll.exam.music_payments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicPaymentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicPaymentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ll/exam/music_payments/MusicPaymentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.exam.music_payments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicPaymentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
