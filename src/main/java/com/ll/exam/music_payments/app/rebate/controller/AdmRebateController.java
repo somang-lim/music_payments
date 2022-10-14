c67a972f1bc4f39d36fbb0bfbfe70fa7088bd8b8
somang-lim <somangjuha@gmail.com>
2022-10-14 10:10:33 +0900

    입력된 월에 해당하는 날짜 범위 구하기

--- src/main/java/com/ll/exam/music_payments/app/rebate/controller/AdmRebateController.java
+++ src/main/java/com/ll/exam/music_payments/app/rebate/controller/AdmRebateController.java
@@ -1,9 +1,14 @@
 package com.ll.exam.music_payments.app.rebate.controller;
 
+import com.ll.exam.music_payments.util.Ut;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.time.LocalDateTime;
 
 @Controller
 @RequestMapping("/adm/rebate")
@@ -12,5 +17,19 @@ public class AdmRebateController {
     @PreAuthorize("hasAuthority('ADMIN')")
     public String showMakeData() {
         return "adm/rebate/makeData";
+    }
+
+    @PostMapping("/makeData")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    @ResponseBody
+    public String makeDat(String yearMonth) {
+        int monthEndDay = Ut.date.getEndDayOf(yearMonth);
+
+        String fromDateStr = yearMonth + "-01 00:00:00.000000";
+        String toDateStr = yearMonth + "-%02d 23:59:59.999999".formatted(monthEndDay);
+        LocalDateTime fromDate = Ut.date.parse(fromDateStr);
+        LocalDateTime toDate = Ut.date.parse(toDateStr);
+
+        return "fromDateStr : %s<br>toDateStr : %s".formatted(fromDateStr, toDateStr);
     }
 }
